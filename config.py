c7d422725aa05ddea121c407381cf6844f55774b
ash <ash@theash.xyz>
2022-09-30 11:37:55 -0700

    add in config.py

---
+++ config.py
@@ -0,0 +1,15 @@
+# get these from https://developer.twitter.com/en/portal/dashboard (use a standalone v1.1 app)
+
+twitter_api_key = "xxx"
+twitter_secret_key = "xxx"
+twitter_access_key = "xxx"
+twitter_access_secret = "xxx"
+
+# get this from @BotFather on telegram
+telegram_secret_key = "xx"
+
+# if you don't know this, @userinfobot on telegram will get this for you
+telegram_user_id = 12345678
+
+# this is the output directory for your images
+output_dir = "cat"
