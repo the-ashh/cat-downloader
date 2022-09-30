b19cc28159b4c344309cc52463b56c75ab46dfe6
ash <ash@theash.xyz>
2022-09-30 12:36:38 -0600

    added video support

--- main.py
+++ main.py
@@ -1,25 +1,43 @@
-import secrets
+import config
 import tweepy
 import wget
 import os
 import re
 import telebot
 
-output_dir = "cat"
+
+if not os.path.exists(config.output_dir):
+    os.makedirs(config.output_dir)
 
 twitter_auth = tweepy.OAuth1UserHandler(
-    secrets.twitter_api_key, secrets.twitter_secret_key, secrets.twitter_access_key, secrets.twitter_access_secret
+    config.twitter_api_key, config.twitter_secret_key, config.twitter_access_key, config.twitter_access_secret
 )
 
 twitter_api = tweepy.API(twitter_auth)
 
-bot = telebot.TeleBot(secrets.telegram_secret_key, parse_mode=None)
+bot = telebot.TeleBot(config.telegram_secret_key, parse_mode=None)
 
 def get_media_link(id):
     tweet = twitter_api.get_status(id)._json
-    
-    return(tweet['entities']['media'][0]['media_url'])
 
+    if "video_info" in tweet["extended_entities"]["media"][0]:
+        vid_dict = {}
+        big_bitrate = 0
+        big_vid = 0
+        j = 0
+        for i in tweet["extended_entities"]["media"][0]["video_info"]["variants"]:
+            if i["content_type"] == "video/mp4":
+                vid_dict[j] = i
+                j += 1
+        for i in vid_dict:
+            if vid_dict[i]["bitrate"] > big_bitrate:
+                big_bitrate = vid_dict[i]["bitrate"]
+                big_vid = i
+        return(vid_dict[big_vid]["url"])
+
+    else: 
+        return(tweet['entities']['media'][0]['media_url'])
+    
 def download_and_sort_cat(url, output):
     download_name = wget.download(url, output)
     file_extension = os.path.splitext(download_name)[1]
@@ -42,15 +60,15 @@ def help(message):
 	bot.reply_to(message, "if u need to type /help, this bot is not for u, srry bestie <3")
 
 @bot.message_handler(func=lambda m: True)
-def echo_all(message):
-    if not message.from_user.id == 316283832:
+def bot(message):
+    if not message.from_user.id == config.telegram_user_id:
         bot.reply_to(message, "srry bestie, this bot is not for you <3")
         return
     
     if "https://twitter.com" in message.text:
         tweet_id = get_tweet_id(message.text)
         tweet_media_link = get_media_link(tweet_id)
-        cat_name = download_and_sort_cat(tweet_media_link, output_dir)
+        cat_name = download_and_sort_cat(tweet_media_link, config.output_dir)
         bot.reply_to(message, "downloaded the cat as " + cat_name + ". enjoy :D")
     else:
         bot.reply_to(message, "not a twitter link, sorry D:")
