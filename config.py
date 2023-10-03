a57d344e22db9ec5351ba15c1a42e14abf2a20ee
Santhosh965531 <146758167+Santhosh965531@users.noreply.github.com>
2023-10-03 14:40:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,15 +1,15 @@
 # get these from https://developer.twitter.com/en/portal/dashboard (use a standalone v1.1 app)
 
-twitter_api_key = "xxx"
-twitter_secret_key = "xxx"
-twitter_access_key = "xxx"
-twitter_access_secret = "xxx"
+twitter_api_key = "****CqJMg1"
+twitter_secret_key = "****CqJMg1"
+twitter_access_key = "1584973946058723328-Mfg8q6RpOVXfBw4AUf10cMazUZ144A"
+twitter_access_secret = "9tIWcyamtro0e9XPMms4bZQbY3GyrwEbQeVGS3whqR6Dz"
 
 # get this from @BotFather on telegram
-telegram_secret_key = "xx"
+telegram_secret_key = "6665320586:AAGzhTvk1OlPtpS6JEpyCg6TqP4oHmyuyGY"
 
 # if you don't know this, @userinfobot on telegram will get this for you
-telegram_user_id = 12345678
+@santhoshbmka = 992264508
 
 # this is the output directory for your images
-output_dir = "cat"
+output_dir = "https://webogram.org/a/#6665320586"
